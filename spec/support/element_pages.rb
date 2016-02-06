265117f6181b574afcd9c77527daf99eee4e395d
OPetriv <oleh.petriv91@gmail.com>
2016-02-06 22:32:08 +0200

    fix

--- spec/support/element_pages.rb
+++ spec/support/element_pages.rb
@@ -2,16 +2,12 @@ Dir[Rails.root.join("spec/pages/*_page.rb")].each{ |f| require f }
 
 module ElementsPages
  
-
   class Navigation
     include Capybara::DSL
 
     def elements_page
       Main.new
-    end
- 
-      
+    end  
   end
-
 end
 
