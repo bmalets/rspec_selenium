350b7a76fcfec96479fdcfebe1aa518ebe72b634
OPetriv <oleh.petriv91@gmail.com>
2016-02-07 00:29:12 +0200

    basic auth spec

--- spec/elements/basicauth_spec.rb
+++ spec/elements/basicauth_spec.rb
@@ -1,1 +1,21 @@
-basicauth_spec.rb
+require 'spec_helper'
+
+describe 'basic Auth'  do
+
+	it 'Negative auth' do
+		@page = ElementsPages::Navigation.new
+
+		mainurl = ENV['SERVER']
+		visit mainurl
+
+		expect(@page.elements_page.main.basicauth[:href]).to eq(mainurl+"/basic_auth")
+		expect(@page.elements_page.main.basicauth.text).to eq("Basic Auth")
+
+		visit "http://admin:admin@the-internet.herokuapp.com/basic_auth"
+
+		expect(@page.basicauth_page.title.text).to eq("Basic Auth")
+
+		expect(@page.basicauth_page.content.text).to eq("Congratulations! You must have the proper credentials.")
+	end
+end
+
