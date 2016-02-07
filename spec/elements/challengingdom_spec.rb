14975751d2558092f8336f4f338311289841de6d
OPetriv <oleh.petriv91@gmail.com>
2016-02-07 19:45:22 +0200

    finish spec

--- spec/elements/challengingdom_spec.rb
+++ spec/elements/challengingdom_spec.rb
@@ -10,5 +10,33 @@ describe 'Challenging DOM'  do
 		expect(@page.elements_page.main.dom[:href]).to eq(mainurl+"/challenging_dom")
 		expect(@page.elements_page.main.dom.text).to eq("Challenging DOM")
 
+		@page.elements_page.main.dom.click
+
+		expect(@page.challengingdom_page.title.text).to eq("Challenging DOM")
+		
+		all(:xpath, "//*[contains(@class,\"button\")]")[0].click
+
+		@page.challengingdom_page.button_alert.click
+
+		@page.challengingdom_page.button_success.click
+
+		all(:xpath, "//*[contains(@class,\"button\")]")[0].click
+
+		@page.challengingdom_page.button_alert.click
+
+		@page.challengingdom_page.button_success.click
+
+		expect(@page.challengingdom_page.line1.text).to eq("Iuvaret0 Apeirian0 Adipisci0 Definiebas0 Consequuntur0 Phaedrum0 edit delete")
+
+		@page.challengingdom_page.line3_edit.click
+
+		expect(current_url).to eq(mainurl+"/challenging_dom#edit")
+
+		@page.challengingdom_page.line8_delete.click
+
+		expect(current_url).to eq(mainurl+"/challenging_dom#delete")
+
+		expect(@page.challengingdom_page.has_canvas?).to be true
+
 	end
 end
